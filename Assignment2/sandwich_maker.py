d0f39bd3cb797c4235f5718da710f9e28a51b893
Gvivero38 <georgeviveros38@gmail.com>
2023-02-06 22:48:02 -0500

    new change

--- Assignment2/sandwich_maker.py
+++ Assignment2/sandwich_maker.py
@@ -4,8 +4,12 @@ class SandwichMaker:
         self.machine_resources = resources
 
     def check_resources(self, ingredients):
-        """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        #checks for ingredients and compares it to resources, false if ingredients is more than resources
+        if(ingredients>= self.machine_resources):
+            return False
+        else:
+            return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        self.sandwich_size = sandwich_size
+        self.order_ingredients = order_ingredients
