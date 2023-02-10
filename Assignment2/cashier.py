801db60baffb2838d98a2312ef0e3fed7d3ad92a
Gvivero38 <georgeviveros38@gmail.com>
2023-02-09 21:02:23 -0500

    process coin complete

--- Assignment2/cashier.py
+++ Assignment2/cashier.py
@@ -6,8 +6,18 @@ class Cashier:
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
         ###
+        print("Please insert coins")
+        total = int(input("How many dollars?: ")) * 1
+        total += int(input("How many half dollars?: ")) * 0.5
+        total += int(input("How many quarters?: ")) * 0.25
+        total += int(input("How many dimes?: ")) * 0.10
+        total += int(input("How many nickels?: ")) * 0.05
+        total += int(input("How many pennies?: ")) * 0.01
+
+
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
         ##
+
