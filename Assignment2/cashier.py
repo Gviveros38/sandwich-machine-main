74d773a082d97e6af3ee76ef1d47c57bd41fe0d0
Gvivero38 <georgeviveros38@gmail.com>
2023-02-09 21:05:30 -0500

    transaction result complete

--- Assignment2/cashier.py
+++ Assignment2/cashier.py
@@ -13,11 +13,17 @@ class Cashier:
         total += int(input("How many dimes?: ")) * 0.10
         total += int(input("How many nickels?: ")) * 0.05
         total += int(input("How many pennies?: ")) * 0.01
-
+        return total
 
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
         ##
-
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            print(f"Here is ${change} in change")
+            return True
+        else:
+            print("Sorry thats not enough money. Money refunded")
+            return False
